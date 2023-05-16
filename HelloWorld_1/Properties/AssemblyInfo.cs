815b6406cda8b278aeb1d7d4a86ba7e0c046a375
Jonathan Mechling <Jmechling88@gmail.com>
2023-05-16 14:31:03 -0400

    Add project files.

---
+++ HelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5B290006-2162-4EDB-BA62-A6B9FC38B3A1")]
+[assembly: AssemblyVersion("1.0.0.0")]
